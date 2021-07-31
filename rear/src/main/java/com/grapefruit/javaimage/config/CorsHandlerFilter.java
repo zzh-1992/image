e9c96df56ee6e24b7a6a0267242e89cc2c8a5142
zzh-1992 <ezzh1992@icloud.com>
2021-08-01 07:45:38 +0800

    后端跨域处理

---
+++ rear/src/main/java/com/grapefruit/javaimage/config/CorsHandlerFilter.java
@@ -0,0 +1,41 @@
+package com.grapefruit.javaimage.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zhihuangzhang
+ */
+@Component
+public class CorsHandlerFilter implements Filter {
+
+    // spring文档相关链接:https://docs.spring.io/spring-framework/docs/current/reference/html/web.html#mvc-cors
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Access-Control-Allow-Credentials", "true");
+        res.addHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
+        res.addHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
+        //res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN," + "Access-Control-Allow-Origin");
+        res.addHeader("Access-Control-Allow-Headers","*");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+}
