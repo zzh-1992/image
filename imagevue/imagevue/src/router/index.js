30e63886c27bb0682486da59ee949ced08f01685
zzh-1992 <ezzh1992@icloud.com>
2021-08-10 23:10:04 +0800

    添加后台路由（嵌套路由）

--- imagevue/imagevue/src/router/index.js
+++ imagevue/imagevue/src/router/index.js
@@ -1,22 +1,44 @@
 // 该文件专门用于创建整个应用容器的路由器
-
 import VueRouter from 'vue-router'
 
 // 引入组件
-import About from "../components/About";
+import HomePage from "../layout/HomePage";
+import Signup from "../layout/Signup";
+
 import Home from "../components/Home";
-import Signup from "../components/Signup";
+import Blog from "../components/Blog";
+import TechnicalStandard from "../components/TechnicalStandard";
+import Video from "../components/Video";
+import Article from "../components/Article";
 
 // 创建并暴露一个路由器
-export default new VueRouter({
+const router =  new VueRouter({
   routes: [
     {
-      path: '/about',
-      component: About
-    },
-    {
-      path: '/home',
-      component: Home
+      path: '/homepage',
+      redirect: '/homepage/home',
+      component: HomePage, children: [
+        {
+          path: 'home',
+          component: Home
+        },
+        {
+          path: 'blog',
+          component: Blog
+        },
+        {
+          path: 'technicalstandard',
+          component: TechnicalStandard
+        },
+        {
+          path: 'video',
+          component: Video
+        },
+        {
+          path: 'article',
+          component: Article
+        },
+      ]
     },
     {
       path: '/',
@@ -24,4 +46,24 @@ export default new VueRouter({
     },
   ]
 })
+
+// 全局前置守卫
+router.beforeEach(function (to, from, next){
+  if(to.path === '/'){
+    // 注册登陆页面直接放行
+    next();
+  }else {
+    const token = localStorage.getItem('token')
+    // 后台页面
+    if(token){
+      // 有登陆或注册成功，后端返回token，允许跳转后台
+      next();
+    }else {
+      // 无token，回到注册登陆页面
+      next('/');
+    }
+  }
+})
+
+export default router;
 
